2d80c98cc3ef447a1194364333806419bf62e3a1
ahmetkasimnazli <ahmetnazl2@gmail.com>
2023-03-07 20:23:06 +0300

    Initial Commit

---
+++ ErasmusApp/ErasmusAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ErasmusAppApp.swift
+//  ErasmusApp
+//
+//  Created by Ahmet Kasım Nazlı on 7.03.2023.
+//
+
+import SwiftUI
+
+@main
+struct ErasmusAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
